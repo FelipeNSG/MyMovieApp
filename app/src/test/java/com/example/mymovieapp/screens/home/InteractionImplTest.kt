7069965cbc534b184f79276ab8a6fdf5cc542e80
Felipe <felipe.sanchez.innotechlabs@gmail.com>
2024-02-23 15:41:26 -0600

    Commit 10 mvp

--- app/src/test/java/com/example/mymovieapp/screens/home/InteractionImplTest.kt
+++ app/src/test/java/com/example/mymovieapp/screens/home/InteractionImplTest.kt
@@ -2,47 +2,126 @@ package com.example.mymovieapp.screens.home
 
 import com.example.mymovieapp.data.repository.MoviesRepository
 import com.example.mymovieapp.movies.Movie
+import com.example.mymovieapp.movies.Series
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.mockk
-import io.mockk.slot
-import io.mockk.spyk
-import kotlinx.coroutines.runBlocking
+import io.mockk.mockkObject
+import io.mockk.unmockkObject
+import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
 class InteractionImplTest {
-    private val moviesRepository: MoviesRepository = mockk()
     private lateinit var interaction: InteractionImpl
-    private var slotMovie = slot<(List<Movie>) -> Unit>()
-
+    private val result: List<Movie> = (
+            listOf(
+                Movie(id = 0, title = "title1", url = "", backdropPath = "", type = "movie"),
+                Movie(id = 0, title = "title2", url = "", backdropPath = "", type = "movie"),
+                Movie(id = 0, title = "title3", url = "", backdropPath = "", type = "movie"),
+                Movie(id = 0, title = "title4", url = "", backdropPath = "", type = "movie"),
+                Movie(id = 0, title = "title5", url = "", backdropPath = "", type = "movie")
+            )
+            )
+    private val resultSeries: List<Series> =
+        listOf(
+            Series(
+                id = 9888,
+                title = "run",
+                url = "http://www.bing.com/search?q=egestas",
+                backdropPath = "elite",
+                type = "series"
+            )
+        )
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-        interaction = spyk()
+        interaction = InteractionImpl()
+        mockkObject(MoviesRepository)
+
     }
 
     @Test
-    fun fetchUpcomingMovies() {
-        runBlocking {
-
-             var movieList: List<Movie> = emptyList()
-            coEvery {
-                moviesRepository.getUpcomingMovies()
-            }answers {
-                movieList
-            }
-
-            interaction.fetchUpcomingMovies {
-                print("movie size ${it.size}")
-            }
+    fun fetchUpcomingMovies() = runTest {
+        //arrange
+        coEvery {
+            MoviesRepository.getUpcomingMovies()
+        } coAnswers {
+            result
         }
+        //act
+        interaction.fetchUpcomingMovies {
+            //assertion
+            Assert.assertEquals(5, it.size)
+        }
+    }
 
+    @Test
+    fun fetchMostPopularMovies() = runTest {
+        //arrange
+        coEvery {
+            MoviesRepository.getPopularMovies()
+        } coAnswers {
+            result
+        }
+        //act
+        interaction.fetchMostPopularMovies {
+            println("Hello")
+            //assertion
+            Assert.assertEquals(5, it.size)
+        }
+    }
+
+    @Test
+    fun fetchPlayNowMovies() = runTest {
+        //arrange
+        coEvery {
+            MoviesRepository.getPlayNow()
+        } coAnswers {
+            result
+        }
+        //act
+        interaction.fetchPlayNowMovies {
+            //assertion
+            Assert.assertEquals(5, it.size)
+        }
+    }
+
+    @Test
+    fun fetchTopRateMovies() = runTest {
+        //arrange
+        coEvery {
+            MoviesRepository.getTopRate()
+        } coAnswers {
+            result
+        }
+        //act
+        interaction.fetchTopRateMovies {
+            //assertion
+            Assert.assertEquals(5, it.size)
+        }
+    }
+
+    @Test
+    fun fetchMostPopularSeries() = runTest {
+        //arrange
+        coEvery {
+            MoviesRepository.getPopularSeries()
+        } coAnswers {
+            resultSeries
+        }
+        //act
+        interaction.fetchMostPopularSeries {
+            print("hola")
+            //assertion
+            Assert.assertEquals(1, it.size)
+        }
     }
 
     @After
     fun tearDown() {
+        unmockkObject(MoviesRepository)
     }
 }
