26c3cd14a77e9b9d4deecfa5fa7b8338c85c851f
Felipe <felipe.sanchez.innotechlabs@gmail.com>
2024-02-29 13:33:22 -0600

    Commit 16 mvp

--- app/src/main/java/com/example/mymovieapp/MainActivity.kt
+++ app/src/main/java/com/example/mymovieapp/MainActivity.kt
@@ -12,7 +12,6 @@ import com.example.mymovieapp.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
